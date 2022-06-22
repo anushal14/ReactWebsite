9b7edd7374e8a6e16df530b20c01daee1288105b
Anushal14 <anushal0483@gmail.com>
2022-06-22 15:27:26 +0530

    initial commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import Back from "../common/back/Back"
+import "./contact.css"
+
+const Contact = () => {
+  const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7680.499575700167!2d75.85811737412786!3d11.367239917688984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba66785d674a927%3A0x9a81f2e9ec163c2d!2sNarikkuni%2C%20Kerala%20673585%2C%20India!5e1!3m2!1sen!2sus!4v1655386440161!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"'
+  return (
+    <>
+      <Back title='Contact us' />
+      <section className='contacts padding'>
+        <div className='container shadow flexSB'>
+          <div className='left row'>
+            <iframe src={map}></iframe>
+          </div>
+          <div className='right row'>
+            <h1>Contact us</h1>
+            <p>We're open for any suggestion or just to have a chat</p>
+
+            <div className='items grid2'>
+              <div className='box'>
+                <h4>ADDRESS:</h4>
+                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
+              </div>
+              <div className='box'>
+                <h4>EMAIL:</h4>
+                <p> info@yoursite.com</p>
+              </div>
+              <div className='box'>
+                <h4>PHONE:</h4>
+                <p> + 1235 2355 98</p>
+              </div>
+            </div>
+
+            <form action=''>
+              <div className='flexSB'>
+                <input type='text' placeholder='Name' />
+                <input type='email' placeholder='Email' />
+              </div>
+              <input type='text' placeholder='Subject' />
+              <textarea cols='30' rows='10'>
+                Create a message here...
+              </textarea>
+              <button className='primary-btn'>SEND MESSAGE</button>
+            </form>
+
+            <h3>Follow us here</h3>
+            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Contact
